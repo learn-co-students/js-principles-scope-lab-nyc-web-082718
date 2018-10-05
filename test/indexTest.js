d663df3367d52836fa7aa1a38facb9e9b01a6a52
github username <github email address>
2018-10-05 17:02:54 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,15 +7,7 @@ describe('Fix the Scope', function() {
     });
   });
 
-  describe('upperCaseCustomerName()', function() {
-    it('modifies the customerName variable', function() {
-      expect(window.customerName).to.equal('bob');
 
-      upperCaseCustomerName();
-
-      expect(window.customerName).to.equal('BOB');
-    });
-  });
   describe('setBestCustomer()', function() {
     it('setBestCustomer', function() {
       expect(window.bestCustomer).to.equal(undefined);
